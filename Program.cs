a64204626a6d371afb172b64f0f82cace4ce5896
obiwanconobi <connerpanaro@gmail.com>
2023-02-21 18:25:34 +0000

    fix with visual studio

--- Program.cs
+++ Program.cs
@@ -4,13 +4,14 @@ using Microsoft.Extensions.Configuration.Json;
 using Microsoft.Extensions.Configuration;
 using LinuxApp.Services.Remote;
 using ByteSizeLib;
-// See https://aka.ms/new-console-template for more information
+
+
 Console.WriteLine("Hello, World!");
 
 IHardwareInfo hardwareInfo = new HardwareInfo();
 hardwareInfo.RefreshAll();
 
-var configuration =  new ConfigurationBuilder()
+var configuration = new ConfigurationBuilder()
      .AddJsonFile($"appsettings.json");
 var config = configuration.Build();
 
@@ -24,13 +25,13 @@ DeviceInformationDto deviceInformationDto = new DeviceInformationDto();
 //Register Device - This should either register the new device, or return the machineId for the already registedDevice
 
 DeviceRegistrationService regService = new DeviceRegistrationService();
-var machineId = regService.Register();
+var machineId = await regService.Register();
 
 
 
 //Device Info
 
-deviceInformationDto.MachineGuid = machineId;
+deviceInformationDto.MachineGuid = Guid.Parse(machineId.ToString());
 deviceInformationDto.ClientID = Guid.Parse(config.GetSection("clientId").Value);
 deviceInformationDto.UserID = Guid.NewGuid();
 
@@ -48,19 +49,19 @@ deviceInformationDto.Username = "TestUser";
 
 //Get Cpu Freq and Load
 
-foreach(var cpu in hardwareInfo.CpuList)
+foreach (var cpu in hardwareInfo.CpuList)
 {
     deviceInformationDto.CPUModel = cpu.Name;
     deviceInformationDto.CPUVendor = cpu.Manufacturer;
     deviceInformationDto.CPUCores = Convert.ToInt32(cpu.NumberOfCores);
     deviceInformationDto.CPUCurrentSpeed = cpu.CurrentClockSpeed;
-    
+
     Console.WriteLine(cpu.CurrentClockSpeed);
 }
 
 
 //Get Mem use and free 
-foreach(var mem in hardwareInfo.MemoryList)
+foreach (var mem in hardwareInfo.MemoryList)
 {
     var total = ByteSize.FromBytes(mem.Capacity).GigaBytes;
 
@@ -68,14 +69,14 @@ foreach(var mem in hardwareInfo.MemoryList)
     deviceInformationDto.RamSpeed = Convert.ToInt32(mem.Speed);
     deviceInformationDto.FreeRam = 0;
     deviceInformationDto.RamVendor = "Virtual";
-   // deviceInformationDto.FreeRam = 
+    // deviceInformationDto.FreeRam = 
     //deviceInformationDto.TotalRam = mem.
 }
 
 //Get Disk Information
-foreach(var disk in hardwareInfo.DriveList)
+foreach (var disk in hardwareInfo.DriveList)
 {
-    var driveInfo = new DriveInfo("/");  
+    var driveInfo = new DriveInfo("/");
     var diskInfo = new DiskInformationDto();
     diskInfo.DiskTotalSize = driveInfo.TotalSize;
     diskInfo.DiskFreeSpace = driveInfo.AvailableFreeSpace;
@@ -86,11 +87,11 @@ foreach(var disk in hardwareInfo.DriveList)
     diskInfo.LoggedDateTime = DateTime.Now;
     diskInfo.DiskUsedSpace = driveInfo.TotalSize - driveInfo.AvailableFreeSpace;
     diskInfo.ClientId = Guid.Parse(config.GetSection("clientId").Value);
-    diskInfo.DeviceId = machineId;
+    diskInfo.DeviceId = Guid.Parse(machineId.ToString());
 
     deviceInformationDto.DiskInformation.Add(diskInfo);
-   // var result = driveInfo.DriveFormat;
-  //  diskInfo.IsSSD = driveInfo.DriveType;
+    // var result = driveInfo.DriveFormat;
+    //  diskInfo.IsSSD = driveInfo.DriveType;
     //diskInfo.DiskFreeSpace = disk.
 }
 
@@ -100,11 +101,17 @@ foreach(var disk in hardwareInfo.DriveList)
 
 DeviceLogService logService = new DeviceLogService();
 logService.UploadData(deviceInformationDto);
- 
 
 
 
-foreach(var mb in hardwareInfo.BiosList)
+
+foreach (var mb in hardwareInfo.BiosList)
 {
-        Console.WriteLine(mb.SerialNumber);
+    Console.WriteLine(mb.SerialNumber);
 }
+
+
+
+
+
+// See https://aka.ms/new-console-template for more information
